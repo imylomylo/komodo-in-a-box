812e20b8dc587f28d63a5c2c3cc85a277a834219
imylomylo <imylomylo@gmail.com>
2019-05-30 17:06:45 +0300

    fixed sendalltoaddress

--- setup/komodo-api/wallet.sh
+++ setup/komodo-api/wallet.sh
@@ -64,6 +64,22 @@ function importprivkey {
   fi
 }
 
+
+function sendtoself {
+	KIABMETHOD="sendtoaddress"
+	if ps aux | grep komodod | grep $CHAIN | grep -v grep ; then
+	input_box "Send Question" "How many coins to send, must be less than your balance?" "1" SENDAMOUNT
+	echo $SENDAMOUNT
+	sleep 1
+    curl -s --user $rpcuser:$rpcpassword --data-binary "{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"$KIABMETHOD\", \"params\": [\"$DEVADDRESS\", $SENDAMOUNT, \"\", \"\", true]}" -H 'content-type: text/plain;' http://127.0.0.1:$rpcport/ | jq -r '.result' > ~/.kiabresponse
+    KIABRESPONSE=`cat ~/.kiabresponse`
+    message_box "$KIABMETHOD response" "$KIABRESPONSE"
+  else
+    echo "Nothing to query - start devmode..."
+    sleep 1
+  fi
+}
+
 function sendalltoself {
 	KIABMETHOD="sendtoaddress"
 	if ps aux | grep komodod | grep $CHAIN | grep -v grep ; then
--- setup/wallet-console.sh
+++ setup/wallet-console.sh
@@ -13,8 +13,9 @@ Choose the TASK" 25 120 14 \
 INFO "getwalletinfo - get wallet info for $CHAIN" \
 LIST "listunspent - get unspent outputs for $CHAIN" \
 SENDTOADDRESS "sendtoaddress - send some native $CHAIN coins to an address" \
+SENDTOSELF "sendtoaddress - send some native $CHAIN coins to yourself" \
+SENDALLTO "sendtoaddress - send all native $CHAIN coins to an address" \
 SENDALLSELF "sendtoaddress - send all native $CHAIN coins to yourself" \
-SENDALLADDRESS "sendtoaddress - send all native $CHAIN coins to an address" \
 TOKENADDRESS "tokenaddress - get address information for tokens on $CHAIN for this pubkey" \
 BALANCE "getbalance - get the balance for this node $CHAIN wallet" \
 VALIDATE "validateaddress - validate the address on this $CHAIN node wallet" \
@@ -25,16 +26,17 @@ menuitem=$(<"${INPUT}")
 
 # make decsion
 case $menuitem in
-	LIST) listunspent;;
-	INFO) getwalletinfo;;
-	SENDTOADDRESS) sendtoaddress;;
-    SENDALLSELF) sendalltoself;;
-    SENDALLTO) sendallto;;
-	TOKENADDRESS) tokenaddress;;
-    BALANCE) getbalance;;
-	VALIDATE) validateaddress;;
-	IMPORT-DEV-WALLET) importprivkey;;
-	Back) echo "Back"; break;;
+  LIST) listunspent;;
+  INFO) getwalletinfo;;
+  SENDTOADDRESS) sendtoaddress;;
+  SENDTOSELF) sendtoself;;
+  SENDALLSELF) sendalltoself;;
+  SENDALLTO) sendallto;;
+  TOKENADDRESS) tokenaddress;;
+  BALANCE) getbalance;;
+  VALIDATE) validateaddress;;
+  IMPORT-DEV-WALLET) importprivkey;;
+  Back) echo "Back"; break;;
 esac
 done
 }
