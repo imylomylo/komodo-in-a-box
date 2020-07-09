bec7f0fc5b400280c84244df22721275951c0760
mylo <imylomylo@gmail.com>
2020-07-09 06:50:58 +0000

    oracles workflow (fund, register)

--- setup/cc/oracles.sh
+++ setup/cc/oracles.sh
@@ -72,11 +72,26 @@ function oraclesaddress {
   fi
 }
 
+function oraclesfund {
+  KIABMETHOD="oraclesfund"
+  if ps aux | grep komodod | grep $CHAIN | grep -v grep ; then
+    curl -s --user $rpcuser:$rpcpassword --data-binary "{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"$KIABMETHOD\", \"params\": [\"$ORACLESTXID\"]}" -H 'content-type: text/plain;' http://127.0.0.1:$rpcport/ | jq -r '.result.hex' > ~/.kiabresponse
+    KIABRESPONSE=`cat ~/.kiabresponse`
+    RAWHEX=$KIABRESPONSE
+    sendrawtransaction  
+  else
+    echo "Nothing to query - start $CHAIN..."
+    sleep 1
+  fi
+}
+
 function oraclesregister {
   KIABMETHOD="oraclesregister"
   if ps aux | grep komodod | grep $CHAIN | grep -v grep ; then
     curl -s --user $rpcuser:$rpcpassword --data-binary "{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"$KIABMETHOD\", \"params\": [\"$ORACLESTXID\", \"$ORACLESDATAFEE\"]}" -H 'content-type: text/plain;' http://127.0.0.1:$rpcport/ | jq -r '.result.hex' > ~/.kiabresponse
     KIABRESPONSE=`cat ~/.kiabresponse`
+    cat ~/.kiabresponse
+    sleep 3
     RAWHEX=$KIABRESPONSE
     sendrawtransaction  
   else
--- setup/oracles-console.sh
+++ setup/oracles-console.sh
@@ -13,6 +13,7 @@ LIST "oracleslist - list oracles created on $CHAIN" \
 INFO "oraclesinfo - get info about a oracles on $CHAIN" \
 CREATE "oraclescreate - create a oracles on $CHAIN" \
 ADDRESS "oraclesaddress - get address information on $CHAIN" \
+FUND "oraclesfund - before registering as a publisher must fund $CHAIN" \
 REGISTER "oraclesregister - register as a publisher on an oracle on $CHAIN" \
 Back "Back" 2>"${INPUT}"
 
@@ -24,6 +25,7 @@ case $menuitem in
   INFO) submenu_oraclesinfo;;
   CREATE) submenu_oraclescreate;;
   ADDRESS) oraclesaddress;;
+  FUND) submenu_oraclesfund;;
   REGISTER) submenu_oraclesregister;;
   ASK) submenu_oraclesask;;
   BID) submenu_oraclesbid;;
@@ -32,13 +34,8 @@ esac
 done
 }
 
-function submenu_oraclesregister {
+function submenu_oraclesfund {
 
-    input_box "Data Fee Question" "Data fee for subscribers?" "1000000" ORACLESDATAFEE
-
-    echo $ORACLESDATAFEE
-    sleep 2
-    
     # the rest is the same as oraclesinfo, except instead of calling
     # oraclesinfo, it it calling oraclesregister
     helper_oracleslist
@@ -63,7 +60,6 @@ function submenu_oraclesregister {
     /bin/rm $HOME/.kiabresponse
 
     cat $HOME/.tmp
-
     COUNT=0
     #used later during selection
     CHOICES2="("
@@ -72,6 +68,7 @@ function submenu_oraclesregister {
     for i in `cat $HOME/.tmp`
     do
       echo "Trying $i"
+sleep 1
       ORACLESTXID=$i
       helper_oraclesinfo
       ORACLESNAME=`cat ~/.kiabresponse  | jq -r '. | .name'`
@@ -83,6 +80,8 @@ function submenu_oraclesregister {
     CHOICES2="$CHOICES2)"
     # echo $CHOICES3
     # sleep 1
+
+cat $HOME/.kiabresponse
 
     while true
     do
@@ -113,7 +112,106 @@ function submenu_oraclesregister {
     # echo ${CHOICES4[@]:$menuitem:1}
     ORACLESTXID=${CHOICES4[@]:$menuitem:1}
     # echo $menuitem
-    # echo $ORACLESTXID
+    echo $ORACLESTXID
+    echo "Calling oraclesfund for oracle tx id $ORACLESTXID"
+sleep 3
+    oraclesfund
+    sleep 1
+    done
+}
+
+function submenu_oraclesregister {
+
+    input_box "Data Fee Question" "Data fee for subscribers?" "1000000" ORACLESDATAFEE
+
+    echo $ORACLESDATAFEE
+    sleep 2
+    
+    # the rest is the same as oraclesinfo, except instead of calling
+    # oraclesinfo, it it calling oraclesregister
+    helper_oracleslist
+    # get the array from ~/.kiabresponse
+    # [
+    #   "be75c48e0168668ef8a6861e51ba8461deb5a923bf50bd7e9a89917dfb066462",
+    #   "lkajsdlfjasdlfkjalskdfj"
+    # ]
+    # turn it into a menu item
+    #
+    # 1 - "be75....."
+    # 2 - "lkaj...."
+    #
+
+    echo "Got the list of oracles"
+sleep 3
+    # TODO
+    # add if statement grep for null in ~/.kiabresponse
+    # if responds then message box saying no oracles to list
+    # get info on or interact with, exit to last menu
+
+    cat $HOME/.kiabresponse | jq -r '.[]' > $HOME/.tmp
+    /bin/rm $HOME/.kiabresponse
+
+    cat $HOME/.tmp
+sleep 3
+    COUNT=0
+    #used later during selection
+    CHOICES2="("
+    #used first for dialog
+    CHOICES3=""
+    for i in `cat $HOME/.tmp`
+    do
+      echo "Trying $i"
+sleep 1
+      ORACLESTXID=$i
+      helper_oraclesinfo
+      ORACLESNAME=`cat ~/.kiabresponse  | jq -r '. | .name'`
+      CHOICES2="$CHOICES2 $i "
+      CHOICES3="$CHOICES3 $COUNT $ORACLESNAME"
+      echo "$COUNT $i \\">> $HOME/.kiabresponse
+sleep 1
+      let COUNT=COUNT+1
+    done
+    CHOICES2="$CHOICES2)"
+    # echo $CHOICES3
+    # sleep 1
+
+cat $HOME/.kiabresponse
+sleep 5
+
+    while true
+    do
+    echo "Oracles system for blockchain:"
+sleep 3
+    echo $CHAIN
+    ### display main menu ###
+    dialog --clear  --help-button --backtitle "Cakeshop Console" \
+    --title "[ S T A R T E R - K I T - O R A C L E S - C O N S O L E ]" \
+    --menu "These are the funding txid for the oracles creation. \n\
+    They follow the marker pattern used for creating contracts. \n\
+    The funding tx marks the beginning of this contract and can \n\
+    easily be used to track it throughout the blockchain. \n\
+    You can use the UP/DOWN arrow keys, the first \n\
+    letter of the choice as a hot key, or the \n\
+    number keys 1-9 to choose an option.\n\
+    Choose the TASK" 25 120 14 \
+    $CHOICES3 \
+    Back "Back" 2>"${INPUT}"
+
+    menuitem=$(<"${INPUT}")
+
+    # make decsion break or continue with selection of oraclesinfo
+    case $menuitem in
+        Back) echo "Back"; break;;
+    esac
+
+    declare -a CHOICES4=$CHOICES2
+    # echo ${CHOICES4[@]:$menuitem:1}
+    ORACLESTXID=${CHOICES4[@]:$menuitem:1}
+    # echo $menuitem
+    echo $ORACLESTXID
+    echo $ORACLESDATAFEE
+    echo "Calling oraclesregister with data fee $ORACLESDATAFEE for oracle tx id $ORACLESTXID"
+sleep 3
     oraclesregister
     sleep 1
     done
